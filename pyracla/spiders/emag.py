6894194e1ddcbef98a4650aa9c1b15c65544a62a
Adrian M <adrian.psvmihaila@gmail.com>
2016-11-22 22:43:40 +0200

    Doesn't look like anything to me.

--- pyracla/spiders/emag.py
+++ pyracla/spiders/emag.py
@@ -8,9 +8,9 @@ class EMAGSpider(scrapy.Spider):
     start_urls = ["http://www.emag.ro/placi_video/c"]
 
     def parse(self, response):
-        for item in response.css("div.product-holder-grid"):
+        for item in response.css("div.product-holder-grid form.inner-form"):
             yield {
-                'title': item.css('form.inner-form div.middle-container h2 a::text').extract_first().strip(),
-                'price_ron': '%i.%i' % (item.css("form.inner-form div.bottom-container div.pret-produs-listing span.price-over span.money-int::text").extract_first().strip(),
-                                        item.css("form.inner-form div.bottom-container div.pret-produs-listing span.price-over span.money-decimal::text").extract_first().strip()),
+                'title': item.css("div.middle-container h2 a::text").extract_first(),
+                'price_ron': '%i.%i' % (item.css("div.bottom-container div.pret-produs-listing span.price-over span.money-int::text").extract_first(),
+                                        item.css("div.bottom-container div.pret-produs-listing span.price-over span.money-decimal::text").extract_first()),
             }
