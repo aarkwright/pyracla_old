4efb277c941a6782b8baf11244a434789b109596
Adrian M <adrian.psvmihaila@gmail.com>
2016-11-22 22:25:15 +0200

    Try #3.

--- pyracla/spiders/emag.py
+++ pyracla/spiders/emag.py
@@ -1,14 +1,16 @@
 import scrapy
 
 
-class QuotesSpider(scrapy.Spider):
+class EMAGSpider(scrapy.Spider):
     name = "emag"
 
     allowed_domains = ["emag.ro"]
-    start_urls = ['www.emag.ro/placi_video/c']
+    start_urls = ["http://www.emag.ro/placi_video/c"]
 
     def parse(self, response):
-        for item, price in response.css("div.product-holder-grid form.inner-form"), response.css("div.product-holder-grid form.inner-form div.bottom-container div.pret-produs-listing"):
+        top_level = response.css("div.product-holder-grid form.inner-form")
+        for item, price in top_level.css("form.inner-form"), \
+                           top_level.css("form.inner-form div.bottom-container div.pret-produs-listing"):
             yield {
                 'title': item.css('div.middle-container h2 a::text').extract_first().strip(),
                 'price_ron': '%i.%i' % (price.css("span.price-over span.money-int::text").extract_first().strip(),
