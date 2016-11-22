93c49fe94a27e745cca616c132cd0e80ab1c11cc
Adrian M <adrian.psvmihaila@gmail.com>
2016-11-22 22:27:00 +0200

    Try #4.

--- pyracla/spiders/emag.py
+++ pyracla/spiders/emag.py
@@ -8,9 +8,8 @@ class EMAGSpider(scrapy.Spider):
     start_urls = ["http://www.emag.ro/placi_video/c"]
 
     def parse(self, response):
-        top_level = response.css("div.product-holder-grid form.inner-form")
-        for item, price in top_level.css("form.inner-form"), \
-                           top_level.css("form.inner-form div.bottom-container div.pret-produs-listing"):
+        for item, price in response.css("div.product-holder-grid form.inner-form"), \
+                           response.css("div.product-holder-grid form.inner-form div.bottom-container div.pret-produs-listing"):
             yield {
                 'title': item.css('div.middle-container h2 a::text').extract_first().strip(),
                 'price_ron': '%i.%i' % (price.css("span.price-over span.money-int::text").extract_first().strip(),
