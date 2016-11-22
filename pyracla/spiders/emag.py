da093bb7a6136995108d072f6870380df866948f
Adrian M <adrian.psvmihaila@gmail.com>
2016-11-22 22:49:38 +0200

    Proper replace.

--- pyracla/spiders/emag.py
+++ pyracla/spiders/emag.py
@@ -11,6 +11,6 @@ class EMAGSpider(scrapy.Spider):
         for item in response.css("div.product-holder-grid form.inner-form"):
             yield {
                 'title': item.css("div.middle-container h2 a::text").extract_first(),
-                'price_ron': '{}.{:2d}ron'.format(int(item.css("div.bottom-container div.pret-produs-listing span.price-over span.money-int::text").extract_first().replace("\.")),
+                'price_ron': '{}.{:2d}ron'.format(int(item.css("div.bottom-container div.pret-produs-listing span.price-over span.money-int::text").extract_first().replace("\.", "")),
                                         int(item.css("div.bottom-container div.pret-produs-listing span.price-over span.money-decimal::text").extract_first())),
             }
