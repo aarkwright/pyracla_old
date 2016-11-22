763c028b54761eb1c675cd5d57eaec49490eaf57
Adrian M <adrian.psvmihaila@gmail.com>
2016-11-22 21:24:12 +0200

    Test push.

--- pyracla/spiders/emag.py
+++ pyracla/spiders/emag.py
@@ -1,1 +1,17 @@
-#init
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "emag"
+
+    def start_requests(self):
+        urls = ['www.emag.ro/placi_video/c']
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        page = response.url.split("/")[-2]
+        filename = 'emag-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
