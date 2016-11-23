74fbbe729a77a381ce84e8d1bfb1579740a9f85c
Adrian M <adrian.psvmihaila@gmail.com>
2016-11-23 10:52:24 +0200

    Introduce custom start_requests func.

--- pyracla/spiders/emag.py
+++ pyracla/spiders/emag.py
@@ -1,23 +1,31 @@
-import scrapy
+from scrapy.spider import Spider
+from scrapy.http import Request
 
 
-class EMAGSpider(scrapy.Spider):
+class EMAGSpider(Spider):
     name = "emag"
 
     allowed_domains = ["emag.ro"]
-    start_urls = ["http://www.emag.ro/placi_video/c"]
+    url = ["http://www.emag.ro/placi_video/c"]
+
+    def start_requests(self):
+        yield Request(self.url, callback=self.parse, headers={
+            "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 5_1 like Mac OS X) AppleWebKit/534.46 (KHTML, like Gecko) Version/5.1 Mobile/9B179 Safari/7534.48.3"})
 
     def parse(self, response):
         for item in response.css("div.product-holder-grid form.inner-form"):
             yield {
                 'title': item.css("div.middle-container h2 a::text").extract_first().strip(),
                 'price_ron': float('{}.{}'.format(
-                    item.css("div.bottom-container div.pret-produs-listing span.price-over span.money-int::text").extract_first().replace(".", ""),
-                    item.css("div.bottom-container div.pret-produs-listing span.price-over sup.money-decimal::text").extract_first())),
+                    item.css(
+                        "div.bottom-container div.pret-produs-listing span.price-over span.money-int::text").extract_first().replace(
+                        ".", ""),
+                    item.css(
+                        "div.bottom-container div.pret-produs-listing span.price-over sup.money-decimal::text").extract_first())),
             }
 
         next_page = response.css("div.emg-pagination-box a.emg-icon-holder::attr(href)").extract_first()
         if next_page is not None:
             next_page = response.urljoin(next_page)
             print(next_page)
-            yield scrapy.Request(next_page, callback=self.parse, dont_filter=False)
+            yield Request(next_page, callback=self.parse, dont_filter=False)
