30e7263c98a09af632b31fddec4966f7419f34cf
Adrian M <adrian.psvmihaila@gmail.com>
2016-11-23 14:45:02 +0200

    reworked category/product logic.

--- pyracla/spiders/emag.py
+++ pyracla/spiders/emag.py
@@ -1,42 +1,56 @@
 from scrapy.spiders import Spider
 from scrapy.http import Request
-from scrapy.selector import Selector
 
 
 class EMAGSpider(Spider):
     name = "emag"
 
     allowed_domains = ["emag.ro"]
-    urls = ["http://www.emag.ro/placi_video/c"]
+    urls = ["http://www.emag.ro/"]
 
     def start_requests(self):
         for url in self.urls:
             yield Request(url, callback=self.parse_menu)
 
-    def parse_product(self, reponse):
-        pass
-
     def parse_menu(self, response):
 
-        categs = response.xpath('//nav[@id = "emg-mega-menu"]/ul/li/div/div/div[@class = "emg-megamenu-column"]/a[@class = "emg-megamenu-link"][not(a/@class="emg-megamen-link.is-heading")]')
+        # Get all proper categories [no header categories]
+        categs = response.xpath(
+            '//nav[@id = "emg-mega-menu"]/ul/li/div/div/div[@class = "emg-megamenu-column"]/a[@class = "emg-megamenu-link"][not(a/@class="emg-megamen-link.is-heading")]')
 
-        # href for first (Laptopuri) element:
+        # Get link href for first (Laptopuri) element:
+        #self.results = {}
         for categ in categs:
-            categ.xpath('@href')
+            category_name = categ.xpath('text()').extract_first()
+            category_href = categ.xpath('@href').extract_first()
+            products = yield Request(response.urljoin(category_href), callback=self.parse_categ)
 
-        # get categories
-        navbar = response.css("div.emg-top-menu.emg-fluid-top-menu nav")
-        categs = navbar.css("ul li")
+            #results = {'name': category_name, 'link': category_href, 'products': products}
+
+            yield {
+                'name': category_name,
+                'link': category_href,
+                'products': products,
+            }
 
 
-        # Get the correct next page indicator
-        for _e in response.css("div.emg-pagination-box a.emg-icon-holder"):
-            if len(_e.css("span.icon-i44-go-right").extract()) == 1:  # should be only one Next page indicator.
-                next_page = _e.css("::attr(href)").extract_first()
-            else:
-                next_page = None  # end of the line
+    def parse_categ(self, response):
+
+        products = response.xpath('//div[@class = "product-holder-grid"]/form[@class = "inner-form"]')
+
+        for product in products:
+            yield {
+                'title': product.css("div.middle-container h2 a::text").extract_first().strip(),
+                'price_ron': float('{}.{}'.format(product.css(
+                    "div.bottom-container div.pret-produs-listing span.price-over span.money-int::text").extract_first().replace(
+                    ".", ""), product.css(
+                    "div.bottom-container div.pret-produs-listing span.price-over sup.money-decimal::text").extract_first())), }
+
+        _np = response.xpath(
+            '//div[@id = "emg-pagination-numbers"]/a[@class = "emg-icon-holder"]/span[@class = "icon-i44-go-right"]')
+        next_page = _np.xpath('ancestor::a[@class = "emg-icon-holder"]/@href').extract_first()
 
         if next_page is not None:
             next_page = response.urljoin(next_page)
             print(next_page)
-            yield Request(next_page, callback=self.parse_menu)  # , dont_filter=False)
+            yield Request(next_page, callback=self.parse_categ)  # , dont_filter=False)
